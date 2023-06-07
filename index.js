4ed890da7c2256c2c07be311d017fb9dfa2b5ce5
Matthew Bull <mattbull33@gmail.com>
2023-06-06 23:04:39 -0400

    added renderlogo class constructor

--- index.js
+++ index.js
@@ -18,10 +18,30 @@ const userQuestions = [
         type: "input",
         name: "shapeColor",
         message: "Enter up to 3 characters"
-    }, {
+    }, 
+    {
         type: "list",
         name: "shape",
         message: "Choose a Shape",
         choices: ["Circle", "Triangle", "Square"],
     },
-]
+];
+
+class RenderLogo {
+    constructor(){
+        this.userText = ""
+        this.userShape = ""
+    };
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.userShape}${this.userText}</svg>`
+    };
+
+    setText(text, color){
+        this.userText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    };
+
+    setShape(shape){
+        this.userShape = shape.render();
+
+    };
+};
