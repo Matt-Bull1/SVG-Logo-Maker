b5b1f835d5254a560a20a21fa1bb16cea6a8b5c7
Matthew Bull <mattbull33@gmail.com>
2023-06-06 22:33:46 -0400

    added user questions in index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const {Circle, Triangle, Square} = require('./lib/shapes');
+
+const userQuestions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to 3 characters"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter up to 3 characters"
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter up to 3 characters"
+    }, {
+        type: "list",
+        name: "shape",
+        message: "Choose a Shape",
+        choices: ["Circle", "Triangle", "Square"],
+    },
+]
